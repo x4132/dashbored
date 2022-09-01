f67827b54d4a6f0537084f18298d66cc20a138cf
x4132 <31595285+x4132@users.noreply.github.com>
2022-09-01 14:09:24 -0700

    add logging and sysinfo and stuff

--- client/src/components/System.tsx
+++ client/src/components/System.tsx
@@ -8,11 +8,12 @@ export default function System() {
 
     async function processData() {
         var sysinfo = await (await fetch("/api/sysinfo")).json()
+        console.log(sysinfo);
         setInfo(sysinfo);
     }
 
     useEffect(() => {
-        intervalId = setInterval(() => processData(), 5000);
+        intervalId = setInterval(() => processData(), 1000);
 
         processData();
         return () => clearInterval(intervalId);
@@ -21,8 +22,26 @@ export default function System() {
     return (
         <Block className="w-33" >
             <div className="w-100 text-center" >
-                <h4>{info.hostname ? info.hostname : "System Info"} Info</h4>
+                <h4>{info.hostname ? info.hostname : "System"} Info</h4>
+            </div>
+
+            <div className="row">
+                <StatusDOM>
+                    <h6>CPU: {info.cpu}%</h6>
+                </StatusDOM>
+
+                <StatusDOM>
+                    <h6>RAM: {info.mem?.usedMemPercentage}% Used, {Math.round(info.mem?.usedMemMb / 10) / 100}/{Math.round(info.mem?.totalMemMb / 10) / 100}GB Free</h6>
+                </StatusDOM>
             </div>
         </Block>
+    )
+}
+
+function StatusDOM({ children }: { children: React.ReactNode }) {
+    return (
+        <div className="col d-flex align-items-center">
+            {children}
+        </div>
     )
 }
--- server/index.ts
+++ server/index.ts
@@ -1,11 +1,45 @@
 import express, { Request, Response } from "express";
 import axios from "axios";
-import os from "os";
+import osUtils from "node-os-utils"
 import path from "path";
+import url from "url"
+import fs from "fs";
 const app = express();
 const port = 4000;
 
+const __filename = url.fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+// fun stuff
+const origLog = console.log.bind(console);
+
+if (!fs.existsSync("log.txt")) {
+  fs.writeFileSync("log.txt", "");
+}
+
+var stream = fs.createWriteStream("log.txt");
+
+console.log = (...data) => {
+  var str = "";
+
+  for (var i = 0; i < data.length; i++) {
+    str += data[i];
+    if (i != data.length - 1) {
+      str += " ";
+    } else {
+      str += "\n";
+    }
+  }
+
+  stream.write(str);
+
+  origLog(...data);
+}
+
+// normal code
 app.get("/api/shopifystatus", (req: Request, res: Response) => {
+  console.log(`Shopify Status Query at ${new Date().toUTCString()}`);
+
   axios({
     method: "post",
     headers: {
@@ -44,13 +78,13 @@ app.get("/api/shopifystatus", (req: Request, res: Response) => {
 }`
     }
   })
-    .then(resolve => {
+    .then((resolve: any) => {
       res.status(200);
       res.type("application/json");
       res.end(JSON.stringify(resolve.data.data.status.components));
       return;
     })
-    .catch(reject => {
+    .catch((reject: any) => {
       res.status(500);
       res.end();
       console.error(reject);
@@ -58,42 +92,81 @@ app.get("/api/shopifystatus", (req: Request, res: Response) => {
 });
 
 app.get("/api/amazonstatus", (req: Request, res: Response) => {
+  console.log(`Amazon Status Query at ${new Date().toUTCString()}`);
+
   axios({
     method: "get",
     url: "https://status.aws.amazon.com/currentevents.json"
   })
-    .then(resolve => {
+    .then((resolve: any) => {
       res.status(200);
       res.type("application/json");
       res.end(JSON.stringify(resolve.data));
       return;
     })
-    .catch(reject => {
+    .catch((reject: any) => {
       res.status(500);
       res.end();
       console.error(reject);
     })
 })
 
-app.get("/api/sysinfo", (req: Request, res: Response) => {
-  var output: any = {};
-  output.hostname = os.hostname();
-  output.cpus = os.cpus();
-  output.freemem = os.freemem();
-  output.totalmem = os.totalmem();
-  output.uptime = os.uptime();
 
-  res.status(200);
-  res.type("application/json");
-  res.end(JSON.stringify(output));
+var hostname = osUtils.os.hostname();
+var model = osUtils.cpu.model();
+var count = osUtils.cpu.count();
+
+app.get("/api/sysinfo", (req: Request, res: Response) => {
+  console.log(`System Information Query at ${new Date().toUTCString()}`);
+
+  var output: any = {};
+  osUtils.cpu.usage()
+    .then(resolve => {
+      output.cpu = resolve;
+
+      return osUtils.mem.info();
+    })
+    .then(resolve => {
+      output.mem = resolve;
+
+      return osUtils.netstat.inOut();
+    })
+    .then(resolve => {
+      output.netstat = resolve;
+      output.hostname = hostname;
+      output.model = model;
+      output.count = count;
+
+      res.contentType("application/json");
+      res.status(200);
+      res.json(output);
+      res.end();
+    })
 })
 
 app.use("/assets", express.static("../client/dist/assets/"))
 
 app.get("/", (req: Request, res: Response) => {
-  res.sendFile(path.join(__dirname, "../../client/dist/index.html"))
+  res.sendFile(path.join(__dirname, "../../client/dist/index.html"));
 })
 
 app.listen(port, () => {
   console.log(`Server listening on port ${port}`)
 });
+
+// Closing Actions
+[`exit`, `SIGINT`, `SIGUSR1`, `SIGUSR2`, `uncaughtException`, `SIGTERM`].forEach((eventType) => {
+  process.on(eventType, onClose.bind(null, eventType));
+})
+
+function onClose() {
+  console.log("Shutting down server...");
+  let filesize = fs.statSync("log.txt").size;
+  if (filesize > 1500000) {
+    let buffer = fs.readFileSync("log.txt").toString().slice(filesize - 1000000);
+
+    fs.writeFileSync("log.txt", buffer);
+  }
+
+  process.exit();
+}
